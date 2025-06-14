74d479054ebacfb30b403cf203f123da3e12fcff
Pranjal Goyal <pranjal7852@gmail.com>
2025-06-14 21:42:54 +0530

    fix: resolve docker cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['http://localhost:4200', "http://0.0.0.0:4200"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
